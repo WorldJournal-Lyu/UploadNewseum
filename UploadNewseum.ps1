1cc7bedc544e3726b8040157bea0747d4290821e
WorldJournal-Lyu <lyu@worldjournal.com>
2018-06-19 11:35:22 -0400

    Change mail message

--- UploadNewseum.ps1
+++ UploadNewseum.ps1
@@ -37,6 +37,8 @@ Write-Line -Length 100 -Path $log
 
 
 
+# Set up variables
+
 #$ftp    = Get-WJFTP -Name WorldJournalNewYork
 $ftp    = Get-WJFTP -Name Newseum | Where-Object{$_.User -eq 'ny_wjny'}
 $ePaper = Get-WJPath -Name epaper
@@ -50,7 +52,7 @@ if (!(Test-Path($localTemp))) {New-Item $localTemp -Type Directory | Out-Null}
 
 $localFileName  = "NY" + $workDate + "A01.pdf"
 $localFilePath  = $ePaper.Path + $workDate + "\optimizeda\" + $localFileName
-$localFilePath2 = $localTemp + $localFileName
+$localFilePath2 = $localTemp + (Get-Date).ToString("yyyyMMddHHmmss") + ".pdf"
 $remoteFileName = "NY_WJNY_" + $workDate + ".pdf"
 $remoteFilePath = $ftp.Path + $remoteFileName
 
@@ -59,26 +61,51 @@ Write-Log -Verb "localFilePath" -Noun $localFilePath -Path $log -Type Short -Sta
 Write-Log -Verb "remoteFileName" -Noun $remoteFileName -Path $log -Type Short -Status Normal
 Write-Log -Verb "remoteFilePath" -Noun $remoteFilePath -Path $log -Type Short -Status Normal
 
+
+
+# Upload file from local to Ftp
+# Functions Used: WebClient-UploadFile
+
 Write-Log -Verb "UPLOAD FROM" -Noun $localFilePath -Path $log -Type Long -Status Normal
 Write-Log -Verb "UPLOAD TO" -Noun $remoteFilePath -Path $log -Type Long -Status Normal
 $upload = WebClient-UploadFile -Username $ftp.User -Password $ftp.Pass -RemoteFilePath $remoteFilePath -LocalFilePath $localFilePath
 $mailMsg = $mailMsg + (Write-Log -Verb $upload.Verb -Noun $upload.Noun -Path $log -Type Long -Status $upload.Status -Output String) + "`n"
+if($upload.Status -eq "Bad"){
+    $mailMsg = $mailMsg + (Write-Log -Verb "Exception" -Noun $upload.Exception -Path $log -Type Short -Status $upload.Status -Output String) + "`n"
+}
+
+
+
+# Download file from Ftp to temp from verification
+# Functions Used: WebClient-DownloadFile
 
 Write-Log -Verb "DOWNLOAD FROM" -Noun $remoteFilePath -Path $log -Type Long -Status Normal
 Write-Log -Verb "DOWNLOAD TO" -Noun $localFilePath2 -Path $log -Type Long -Status Normal
 $download = WebClient-DownloadFile -Username $ftp.User -Password $ftp.Pass -RemoteFilePath $remoteFilePath -LocalFilePath $localFilePath2
 $mailMsg = $mailMsg + (Write-Log -Verb $download.Verb -Noun $download.Noun -Path $log -Type Long -Status $download.Status -Output String) + "`n"
-
-Write-Log -Verb "REMOVING" -Noun $localFilePath2 -Path $log -Type Long -Status Normal
-try{
-    $temp = $localFilePath2
-    Remove-Item $localFilePath2 -Force
-    $mailMsg = $mailMsg + (Write-Log -Verb "REMOVE" -Noun "Complete" -Path $log -Type Long -Status Good -Output String)
-}catch{
-    $mailMsg = $mailMsg + (Write-Log -Verb "REMOVE" -Noun "Complete" -Path $log -Type Long -Status Bad -Output String)
+if($download.Status -eq "Bad"){
+    $mailMsg = $mailMsg + (Write-Log -Verb "Exception" -Noun $download.Exception -Path $log -Type Short -Status $download.Status -Output String) + "`n"
 }
 
-if( ($upload.Status -eq 'Bad') -or ($download.Status -eq 'Bad') ){
+
+
+# Delete the downloaded file from temp
+
+Write-Log -Verb "REMOVE" -Noun $localFilePath2 -Path $log -Type Long -Status Normal
+try{
+    $temp = $localFilePath2
+    Remove-Item $localFilePath2 -Force -ErrorAction Stop
+    $mailMsg = $mailMsg + (Write-Log -Verb "REMOVE" -Noun $temp -Path $log -Type Long -Status Good -Output String) + "`n"
+}catch{
+    $mailMsg = $mailMsg + (Write-Log -Verb "REMOVE" -Noun $temp -Path $log -Type Long -Status Bad -Output String) + "`n"
+    $mailMsg = $mailMsg + (Write-Log -Verb "Exception" -Noun $_.Exception.Message -Path $log -Type Short -Status Bad -Output String) + "`n"
+}
+
+
+
+# Set hasError status
+
+if( ($upload.Status -eq "Bad") -or ($download.Status -eq "Bad") ){
     $hasError = $true
 }
 
